3ba6fb8281d29c0421f5bd010a7063c6b695956a
Matt Daniel Brown <developer@mattdanielbrown.com>
2022-10-02 05:25:59 +0000

    Update index.mdx and theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Matt Daniel Brown. All Rights Reserved.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
